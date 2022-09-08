82c1f8d8a72c41d4b15f7894978f7505017b4e3b
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-08 08:31:51 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *  *Description - main - This program prints the size of various types
+ *  *Description - This program prints the size of various types
  *   * on the computer it is compiled and run on.
  *    * Return: Always 0 (Success)
  */
