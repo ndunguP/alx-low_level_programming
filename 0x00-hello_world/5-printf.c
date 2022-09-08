c3ffed0ca53b4b58f702f1dbda50a720f5587f1f
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-08 08:44:03 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - print the string in the put function
-    *Description - the string: with proper grammar, but the outcome is a piece of art,
- *    * followed by a newline, using printf and not puts.
- *     * Return: Always 0 (Success)
- */
+*main - print the string in the put function
+*Description - the string: with proper grammar, but the outcome is a piece of art,
+*followed by a newline, using printf and not puts.
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
