0f77dddf074ccecf147c981b7951b8992b760f65
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-08 07:58:51 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
-include '='<stdio.h>
+include <stdio.h>
 /**
- *  * main - The purpose of this file is to output
- *   * the string: with proper grammar, but the outcome is a piece of art,
+* main - print the string in the put function
+    *Description - the string: with proper grammar, but the outcome is a piece of art,
  *    * followed by a newline, using printf and not puts.
  *     * Return: Always 0 (Success)
  */
