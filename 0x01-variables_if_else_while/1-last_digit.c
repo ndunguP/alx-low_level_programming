c53488ad0830c9a1a0d9a1f4ecafa37e6ab7e419
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-09 06:16:56 -0700

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,6 @@
 #include <time.h>
 
 #include <stdio.h>
-
 /**
  * main - This program outputs a random number to the screen and
  * tells the user what the last digit of the random number is
@@ -21,7 +20,7 @@ if (last_digit > 5)
 printf("Last digit of %d is %d and is greater than 5\n",n, last_digit);
 else
 if (last_digit == 0)
-printf("Last digit of %d is %d and is 0\n", n, last_digit);
+printf("Last digit of %d is %d and is 0\n",n, last_digit);
 else
 printf("Last digit of %d is %d and is less than 6 and not 0\n",n, last_digit);
 return (0);
