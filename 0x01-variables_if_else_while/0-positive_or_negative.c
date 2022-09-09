03c07016796220bb605e22624d1a084aed1dad85
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-09 05:10:13 -0700

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,43 +7,25 @@
 
 
 /**
- *
- *  * main - This program uses the function rand() to output
- *
- *   * random numbers to the screen, then tells the user if it's
- *
- *    * positive, negative, or a zero.
- *
- *     * Return: Always 0 (Success)
- *
- *      */
+ * main - This program uses the function rand() to output
+ * random numbers to the screen, then tells the user if it's*
+ * positive, negative, or a zero.
+ * Return: Always 0 (Success)*
+ */
 
 int main(void)
 
 {
 
-		int n;
-
-
-
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-
-					if (n > 0)
-
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
 								printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
 
-						else if (n == 0)
-
-									printf("%d is zero\n", n);
-
-							else
-
-										printf("%d is negative\n", n);
-
-
-
-								return (0);
-
+else
+										printf("%d is
+										return (0);
 }
