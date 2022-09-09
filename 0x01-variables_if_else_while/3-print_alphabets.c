349cba018be55e8e30e59ce21021e5da906e54bf
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-09 07:05:52 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,31 +6,16 @@
  
  * main - This program prints the alphabet in lowercase
  * and then in uppercase as well.
- *    * Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
-
-		char ch;
-
-
-
-			for (ch = 'a'; ch <= 'z'; ch++)
-
-						putchar(ch);
-
-				for (ch = 'A'; ch <= 'Z'; ch++)
-
-							putchar(ch);
-
-
-
-					putchar('\n');
-
-
-
-						return (0);
-
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
 }
