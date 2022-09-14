140b3d1b92b1ee651d97d6e1a62c0567a4b152bb
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-14 10:33:59 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- 
  * _islower - checks whether the passed character is a lowercase letter
  * @c: The character to check
  * Return: 1 if c is lowercase, otherwise Return: 0
