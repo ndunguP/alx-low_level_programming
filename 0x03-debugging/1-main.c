4477d8585985c0959c8e0336ab9909bc7883fb7d
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-15 06:39:25 -0700

    task0

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,6 +10,7 @@ int main(void)
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
+
 /*
 *while (i < 10)
 *{
@@ -18,4 +19,5 @@ i = 0;
 */
 printf("Infinite loop avoided! \\o/\n");
 return (0);
+
 }
