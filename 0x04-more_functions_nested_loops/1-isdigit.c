722db0a48f3ba06330cf87cf3099aa4419b4e7db
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-16 05:09:36 -0700

    task0

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,24 +1,20 @@
 #include "main.h"
 /**
 * _isdigit - This function checks whether c is an integer 0-9
- *   * @c: The integer to check
- *
- *    * Return: 1 if c is a digit, otherwise 0
- *
- *     */
+* @c: The integer to check
+* Return: 1 if c is a digit, otherwise 0
+*/
 
 int _isdigit(int c)
 
 {
 
-		if (c >= '0' && c <= '9')
+if (c >= '0' && c <= '9')
 
-					return (1);
+return (1);
 
-			else
+else
 
-						return (0);
-
-
+return (0);
 
 }
