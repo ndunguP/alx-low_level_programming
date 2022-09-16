1107d973d6a39e9b0bfb11b771ac4f2d355f8ec8
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-16 14:25:29 -0700

    task7

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *  main - check the code
+ *  Return: Always 0.
+ */
+
+int main(void)
+
+{
+
+void print_diagonal();
+
+print_diagonal(2);
+
+print_diagonal(10);
+
+print_diagonal(-4);
+
+return (0);
+
+}
