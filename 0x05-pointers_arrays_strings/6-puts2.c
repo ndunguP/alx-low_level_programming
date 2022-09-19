7a0b469bd8f45c2a4498ef69d3049da1e58b86ad
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-19 08:32:38 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  puts2 - This function prints one char out of 2 of a string
+ *  followed by a newline
+ *  @str: The string to be evaluated
+ */
+
+void puts2(char *str)
+
+{
+
+		int i;
+
+			char ch;
+
+
+
+				for (i = 0; str[i] != 0; i++)
+
+						{
+
+									if (i % 2 == 0)
+
+												{
+
+																ch = str[i];
+
+																			putchar(ch);
+
+																					}
+
+										}
+
+
+
+					putchar('\n');
+
+}
