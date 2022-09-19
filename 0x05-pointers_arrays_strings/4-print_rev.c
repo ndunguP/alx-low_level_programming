b123a2433127998f57f9675c2eccf585ca148e51
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-19 07:53:01 -0700

    task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,7 +12,7 @@ int i;
 
 char ch;
 
-for (i = 0; s[i] != 0; i++);
+for (i = 0; s[i] != 0; i++)
 
 for (i = i - 1; i >= 0; i--)
 
@@ -20,7 +20,7 @@ for (i = i - 1; i >= 0; i--)
 
 ch = s[i];
 
-putchar(c);
+putchar(ch);
 
 }
 
