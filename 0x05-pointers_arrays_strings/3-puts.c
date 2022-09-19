a71dc7477e5a267947303e902402d3462762cdb4
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-19 07:24:42 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *  _puts - This function prints a string, followed by a newline
+ *  to the standard out
+ *  @str: The string to print out to stdout
+ */
+void _puts(char *str)
+
+{
+
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+ putchar(str[i]);
+
+putchar('\n');
+
+}
