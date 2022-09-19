aeacadb5bdc511493b832c72f55f4fb9fa481203
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-19 09:06:39 -0700

    task8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,24 +10,19 @@ void print_array(int *a, int n)
 
 {
 
-		int i;
+int i;
 
+for (i = 0; i < n; i++)
 
+{
 
-			for (i = 0; i < n; i++)
+printf("%i", a[i]);
 
-					{
+if (i != n - 1)
 
-								printf("%i", a[i]);
+printf(", ");
 
-										if (i != n - 1)
+}
 
-														printf(", ");
-
-											}
-
-
-
-				putchar('\n');
-
+putchar('\n');
 }
