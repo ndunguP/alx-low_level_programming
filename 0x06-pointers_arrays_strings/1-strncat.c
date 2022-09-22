c29746f284a7bb8bfda2091a5016901000cd4162
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-22 06:44:13 -0700

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,41 +9,17 @@
  * @n: The number of bytes to append
  * Return: The concatenated string
  */
-
-
-
 char *_strncat(char *dest, char *src, int n)
-
 {
-
-		int len, i;
-
-
-
-			/* get the length of dest string */
-
-			for (len = 0; dest[len] != 0; len++)
-
-						;
-
-
-
-				/* append src to dest one character at a time */
-
-				for (i = 0; i < n; i++)
-
-						{
-
-									dest[len] = src[i];
-
-											len++;
-
-												}
-
-
-
-					/* return concatenated string */
-
-					return (dest);
-
+int len, i;
+/* get the length of dest string */
+for(len = 0; dest[len] != 0; len++);
+/* append src to dest one character at a time */
+for(i = 0; i < n; i++)
+{
+dest[len] = src[i];
+len++;
+}
+/* return concatenated string */
+return (dest);
 }
