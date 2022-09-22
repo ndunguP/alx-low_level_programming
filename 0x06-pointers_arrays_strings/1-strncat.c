a494750f7d98d169c7c81fd86da21f370043ebc8
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-22 06:50:08 -0700

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,9 +13,9 @@ char *_strncat(char *dest, char *src, int n)
 {
 int len, i;
 /* get the length of dest string */
-for(len = 0; dest[len] != 0; len++);
+for (len = 0; dest[len] != 0; len++);
 /* append src to dest one character at a time */
-for(i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 {
 dest[len] = src[i];
 len++;
