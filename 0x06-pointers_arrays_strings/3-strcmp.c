1321154e22ced887de20813f37da1c37da66a0ae
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-22 03:29:06 -0700

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,29 +11,16 @@
 int _strcmp(char *s1, char *s2)
 
 {
+int i;
 
-		int i;
+/* loop through both strings while s1 & s2 are not null */
 
-
-
-			/* loop through both strings while s1 & s2 are not null */
-
-			for (i = 0; s1[i] != 0 && s2[i] != 0; i++)
-
-					{
-
-								/* find where characters don't match in strings */
-
-								if ((s1[i] - s2[i]) != 0)
-
-												break;
-
-									}
-
-
-
-				/* return difference between those characters */
-
-				return (s1[i] - s2[i]);
-
-}i
+for (i = 0; s1[i] != 0 && s2[i] != 0; i++)
+{
+/* find where characters don't match in strings */
+if ((s1[i] - s2[i]) != 0)
+break;
+}
+/* return difference between those characters */
+return (s1[i] - s2[i]);
+}
