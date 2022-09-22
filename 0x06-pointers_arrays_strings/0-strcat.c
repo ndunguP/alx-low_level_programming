04fdbe2b63eb826d905cffe53554fe7492103950
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-22 07:13:09 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,14 +8,10 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int len, i;
-/* take the length of string dest */
-for (len = 0; dest[len] != 0; len++);
-/* append src string to end of dest string */
-for (i = 0; src[i] != 0; i++)
-{
-dest[len] = src[i];
-len++;
-}
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index]; index++)
+dest[dest_len++] = src[index];
 return (dest);
 }
