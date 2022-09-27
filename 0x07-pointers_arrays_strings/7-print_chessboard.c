e35072f8e475792164c26b4ec96c44f810fb6210
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-09-27 03:30:56 -0700

    task6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_chessboard - prints the chessboard
  * @a: pointer to pieces to print
@@ -8,11 +8,12 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i, j:
-		for (i = 0: i < 8: i++)
+	int i, j;
+		for (i = 0; i < 8; i++)
 		{
 			for (j = 0; j < 8; j++)
-				putchar(a[i][j]);
+			{
+				_putchar(a[i][j]);
 		}
-	putchar('\n');
+	_putchar('\n');
 }
