c7e5da723769a1defa24e02c1379eeeae5d2e398
peter <peter.ndunguoz50@gmail.comgit config --global user.email peter.ndunguoz50@gmail.com>
2022-10-01 02:17:49 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#define UNUSED(argc) (void)(argc)
+
+/**
+* main - This program prints its name, followed by a newline
+* @argc: The number of command line arguments
+* @argv: The string of command line arguments
+* Return: 0 if successful
+*/
+
+int main(int argc, char *argv[])
+{
+UNUSED(argc);
+printf("%s\n", argv[0]);
+return (0);
+}
