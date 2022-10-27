c812267a482f7e595e2d107c875c7120ef98235e
ndunguP <peter.ndunguoz50@gmail.com>
2022-10-27 02:45:08 -0700

    structures_typef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - This struct creates a new type
+* @name: First member, char pointer
+* @age: Second member, float
+* @owner: Third memeber, char pointer
+* Description: This struct has three elements and is a new type dog
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+/**
+* dog - Typdef for dog type
+*/
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
