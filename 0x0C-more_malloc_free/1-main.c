34045a25a5abf5bc8a45bb258eb9ec61564814c1
ndunguP <peter.ndunguoz50@gmail.com>
2022-10-31 03:18:33 -0700

    more

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -8,19 +8,10 @@
 */
 
 int main(void)
-
 {
-
-	    char *concat;
-
-
-
-	        concat = string_nconcat("Best ", "School !!!", 6);
-
-		    printf("%s\n", concat);
-
-		        free(concat);
-
-			    return (0);
-
+char *concat;
+concat = string_nconcat("Best ", "School !!!", 6);
+printf("%s\n", concat);
+free(concat);
+return (0);
 }
