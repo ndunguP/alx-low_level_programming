54886805e90fb3257989a8d1b71a167e42c6e717
ndunguP <peter.ndunguoz50@gmail.com>
2022-12-13 07:55:55 -0800

    dynamics

---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,4 @@
+VimCrypt~03!0g{öWÒª•t‹­X´*ÒÁZ|‚îÿO“'So®îMñJfÏ-â´ÀÊÞƒ¼ëÄˆöìÚYSâŸŠn	´mÈprKŒµÎ«]$ˆÁ­ñ(%
+Âîdm_ü¸â'È­ð×8Òª¾*R*ö[­žQƒü"Ú^æ0ƒWÆK€)«Ò;8¥¡VÍâ¬wµ£+5žÆ
+H)h'Yˆu‘}ý¹?˜RÐ³Ï¯JQ÷#½Äc/Î¥ˆ2«PŠÉõðòÔÁó+ÊæâcâYýJcv™Œ0ìÞßNF¶±ÚBþ¦½÷±íÍ'%KEhÂŸª4cUF›ZrÂ^ÇhYobñä;ÌÐ*}(foHioâiöƒ­zB¢Ž?K.gGª^[2+ÆÏ…ÒÒä·6“ÄÅI’[³{J6‹hñì‹’ÖO,#éx•Ó<`}	W¢ähZ…´(Ü'ƒ ,oœËnÌÇ;³/7“ƒ¼–Ø¼ø9ëGeYöo;¸w­É¼™åäÙaåëÆpÄ0¬`A÷,„¹ª„TâÜVýóI=öIˆS[%uFO§¥îz«‹G|p0(ø¢¼YùWLQ¢–",\äâ¥ñµ®rš°ôîxxX?ÆÞ×\!
+©zœ~DïÒ†ùAåYFXbµ Üvûå§eèmƒ®‚(.aøþ*sm-¿6¼ÛvÈæñ¦ÈÖñH–Š`Ú¦UÈÝë…©–ÀuIÞåÐîùen;àC	ûCŒšpâéÅÓ¨ªt|9T²5šÓ“pïj½’+½ŠŒ*eÒÝ·ºBï³ŠKE÷8oðg·Á—YríÕ›¿Uêež±l³ŒíåžQ¹BþœF~}¨ýp	ÈÀN4å©@Å «ZéVdòß˜ÄbÃî—,k°H^G°?-ÕóžúK—ªŒÏm @þ||cÚÝï)$
