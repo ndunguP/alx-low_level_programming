0e04f3246dda91f5e26703e36b0d147e7904e7bf
ndunguP <peter.ndunguoz50@gmail.com>
2023-01-07 03:35:47 -0800

    hash

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,10 +1,3 @@
-#include "hash_tables.h"
-
-/**
- * hash_djb2 - function that implements the djb2 algorithm
- * @str: pointer
- * Return: hash
- */
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
